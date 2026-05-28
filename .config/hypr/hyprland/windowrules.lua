c700eab4606a1272079596849e6ccaa71221a6ba
Gabriel de Souza <167147017+gsouza38@users.noreply.github.com>
2026-05-28 12:01:58 -0300

    Create windowrules.lua

---
+++ .config/hypr/hyprland/windowrules.lua
@@ -0,0 +1,5 @@
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
